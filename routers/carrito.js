1f1ea460738e9100674d845d72060967e9952fd0
Bautista Cremona <bautista.cremona@coresagroup.com>
2022-05-03 18:30:20 -0300

    Inicio de git --> carrito en progreso

---
+++ routers/carrito.js
@@ -0,0 +1,51 @@
+const { Router } = require("express")
+const router = Router()
+const Carrito = require("../model/carrito")
+
+// --------- layout del carrito ----------
+// { id, timestamp(carrito), productos: { id, timestamp(producto), nombre, descripcion, código, foto (url), precio, stock }
+
+router.post("/", async (req, res) => {
+    const { body } = req
+    console.log(body)
+    await Carrito.crearCarrito(body)
+    res.sendStatus(200)
+})
+  
+  
+router.delete("/:id", (req, res) => res.sendStatus(200))
+
+router.get("/:id/productos", (req, res) => res.sendStatus(200))
+  
+// /api/carrito/2/productos
+router.post("/:id/productos", async (req, res) => {
+    const { id } = req.params
+    const { body } = req
+    console.log(id, body)
+    
+    try {
+      await Carrito.agregarProducto(id, body)
+  
+      res.sendStatus(201)
+    } catch(e) {
+      if (e.message === "no existe") {
+        res.sendStatus(404) // 404 --> Not Found
+      } else {
+        console.log(e)
+        res.sendStatus(500) // 500 --> Error del servidor
+      }
+    }
+})
+  
+router.delete("/:id/productos/:prod", async (req, res) => {
+    const { id, prod } = req.params
+  
+    await Carrito.borrarProducto(id, prod)
+  
+    res.sendStatus(202)
+})
+  
+  
+  
+  
+module.exports = router
